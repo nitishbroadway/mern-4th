b0dad8b0125d8b1cdd69c3b5d8d8052e7618370c
Nitish Uprety <nitish@broadwayinfosys.edu.np>
2024-09-20 09:11:24 +0545

    4th: Third commit

--- api/index.js
+++ api/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const routes = require('./routes')
 
 const app = express()
 
-app.use((req, res) => {
-    res.send('Hello from nodejs')
-})
+app.use(routes)
 
 app.listen(5000, async () => {
     console.log('Server stated at http://localhost:5000')
